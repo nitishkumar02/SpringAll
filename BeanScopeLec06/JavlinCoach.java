efa7e4422f095a232c566db6b0be83990f224fdd
Nitish Kumar <37863742+nitishkumar02@users.noreply.github.com>
2024-08-19 09:51:06 +0530

    JavlinCoach.java

---
+++ BeanScopeLec06/JavlinCoach.java
@@ -0,0 +1,17 @@
+package com.nk.spring;
+
+public class JavlinCoach implements Coach {
+
+	@Override
+	public String getDailyWorkOut() {
+		
+		return null;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return null;
+	}
+
+}
